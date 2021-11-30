d2bca71258addb7818f2f837173c2302bd939e99
BarElbaz1 <barelbaz001694@gmail.com>
2021-11-30 13:03:52 +0200

    EX6

--- CVgrid.js
+++ CVgrid.js
@@ -2,4 +2,16 @@ function thankyou(){
     document.getElementById("button").innerHTML= "barelbaz001694@gmail.com";
     console.log("Bar Elbaz")
     }
+
+
+    function myFunction() {
+        const inpObj = document.getElementById("ID");
+        if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+        } else {
+        document.getElementById("demo").innerHTML = "Input OK";
+        }
+        }
+        
+
     
